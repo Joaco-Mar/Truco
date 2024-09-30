76a3715c87e87d3e8fd1758cfd2f69cfa7c9b711
Joaquin Marolda <joacomarolda@gmail.com>
2024-09-29 22:48:42 -0300

    Base Se cambio texto por iconos, se reordenaron cosas y se planteo una base de elecciones para el jugador más una interfaz de escape con el ctrl+c

--- Truco.py
+++ Truco.py
@@ -6,7 +6,7 @@ def generarCartas(Mazo1, Mazo2):            #Genera las cartas de cada jugador s
     largo=0
     while largo<3:
         Numero=random.choice([i for i in range(1, 13) if i not in [8, 9]])       #Se elige un numero al azar entre 1 y 12, sin incluir el 8 y el 9
-        Palo=random.choice(["Espada","Basto","Oro","Copa"])                      #Se elige un palo al azar
+        Palo=random.choice(["⚔️","🪵","🪙","🍷"])                      #Se elige un palo al azar
         Mazo={"Numero":[Numero], "Palo":[Palo]}
         valido=True
         i=0
@@ -26,18 +26,45 @@ def generarCartas(Mazo1, Mazo2):            #Genera las cartas de cada jugador s
             largo+=1
     return Mazo1
 
+def jugar(mano,ManoJugador, ManoNPC):
+    try:
+        if mano%2==0:
+            print("Jugada del Jugador:/n","Ingrese la carta de quieres jugar 1 -",len(ManoJugador["Numero"]), ":",end="")
+            carta=int(input())
+            while carta<1 or carta>len(ManoJugador["Numero"]):
+                print("Carta invalida, ingrese una carta", ":", end="")
+                carta=int(input())
+    except KeyboardInterrupt:
+        print("\nGracias por jugar")
+        exit()
+        
+
 def main():
-    ManoJugador={"Numero":[], "Palo":[]}
-    ManoNPC={"Numero":[], "Palo":[]}
-    ManoJugador=generarCartas(ManoJugador, ManoNPC)
-    ManoNPC=generarCartas(ManoNPC, ManoJugador)
-    lenght=len(ManoJugador["Numero"])
-    print("Mano del Jugador:")
-    for n in range(lenght):
-        print(ManoJugador["Numero"][n],ManoJugador["Palo"][n])
-    print("\nMano del NPC:")
-    for n in range(lenght):
-        print(ManoNPC["Numero"][n],ManoNPC["Palo"][n])
+    puntajeJugador=0
+    puntajeNPC=0
+    mano=0
+    modoAdmin=input("¿Desea jugar en modo administrador? (S/N):")
+
+    while puntajeJugador<15 and puntajeNPC<15:
+        ManoJugador={"Numero":[], "Palo":[]}
+        ManoNPC={"Numero":[], "Palo":[]}
+
+        ManoJugador=generarCartas(ManoJugador, ManoNPC)
+        ManoNPC=generarCartas(ManoNPC, ManoJugador)
+
+        lenght=len(ManoJugador["Numero"])
+        print("Mano del Jugador:")
+        for n in range(lenght):
+            print(ManoJugador["Numero"][n],ManoJugador["Palo"][n])
+        if modoAdmin=="S":
+            print("\nMano del NPC🤖:")
+            for n in range(lenght):
+                print(ManoNPC["Numero"][n],ManoNPC["Palo"][n])            
+
+        jugar(mano,ManoJugador, ManoNPC)
+
+        print("Puntaje Jugador:", puntajeJugador)
+        print("Puntaje NPC🤖:", puntajeNPC)
 
 
 
